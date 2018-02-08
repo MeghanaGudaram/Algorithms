c27d40951df7bbd396084b44609394c184f63ff4
MeghanaGudaram <meghaleo92@gmail.com>
2018-02-08 16:54:01 -0500

    Update CountingSort.c

--- SortingAlgorithm/CountingSort.c
+++ SortingAlgorithm/CountingSort.c
@@ -36,10 +36,10 @@ void BigOn(int *arr, int size){
 int main(){
 	int array[50], n=0;
 	printf("Enter number of elements in array:\n");
-	scanf("%x", &n);
+	scanf("%d", &n);
 	printf("Enter the elements:\n");
 	for(int i=0; i < n; i++){
-		scanf("%x", &array[i]);
+		scanf("%d", &array[i]);
 	}
 	printf("Before Searching\n");
 	for(int i=0; i < n; i++){
